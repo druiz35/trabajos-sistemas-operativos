5d83045f228c70b54022c3d5d5dd16ec8f204687
Druiz <67380938+druiz35@users.noreply.github.com>
2022-11-27 19:23:12 -0500

    Añadido TLB

--- parcial2resuelto.c
+++ parcial2resuelto.c
@@ -1,27 +1,70 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#define N 5
 
 int main(void) {
-    int direccionesVirtuales[20];
-    int paginas[20];
-    int desplazamientos[20];
+    int direccionesVirtuales[N];
+    int paginas[N];
+    int desplazamientos[N];
+    int *dV = malloc(sizeof(direccionesVirtuales));
     int *p = malloc(sizeof(paginas));
+    int *desp = malloc(sizeof(desplazamientos));
+    int TLBSize = 0;
     while(1) {
-        char input[5];
-        int dirVirtual;
+        char input[20];
         printf("Ingrese direccion virtual: ");
         scanf("%s", input);
         if (strcmp(input, "s") == 0) {
             printf("Good bye!\n");
+            free(dV);
+            free(p);
+            free(desp);
             break;
         }
         else {
-            dirVirtual = atoi(input);
-            int desp = dirVirtual % 4096;
-            int pagina = (dirVirtual - desp) / 4096;
-            printf("Numero de pagina: %d\n", pagina);
-            printf("Desplazamiento: %d\n\n", desp);
+            int dirVirtual = atoi(input);
+            int desp = 0, pagina = 0, index;
+            int TLBState = 0; // 0 para MISS, 1 para HIT
+            for (index = 0; index<N; index++) {
+                if (direccionesVirtuales[index] == dirVirtual) {
+                    desp = desplazamientos[index];
+                    pagina = paginas[index];
+                    TLBState = 1;
+                    break;
+                }
+            }
+            if (desp == 0 && pagina == 0) {
+                desp = dirVirtual % 4096;
+                pagina = (dirVirtual - desp) / 4096;
+                // Se usa la política FIFO para el reemplazo
+                if (TLBSize == N) {
+                    for (index = 0; index < N-1; index++) {
+                        direccionesVirtuales[index] = direccionesVirtuales[index + 1];
+                        paginas[index] = paginas[index + 1];
+                        desplazamientos[index] = desplazamientos[index + 1];
+                    }
+                    direccionesVirtuales[N-1] = dirVirtual;
+                    paginas[N-1] = pagina;
+                    desplazamientos[N-1] = desp;
+                }
+                else {
+                    direccionesVirtuales[TLBSize] = dirVirtual;
+                    paginas[TLBSize] = pagina;
+                    desplazamientos[TLBSize] = desp;
+                    TLBSize = TLBSize + 1;
+                }
+            }
+            if (TLBState == 0) {
+                printf("Numero de pagina: %d\n", pagina);
+                printf("Desplazamiento: %d\n", desp);
+                printf("TLB: Miss\n\n");
+            }
+            else if (TLBState == 1) {
+                printf("Numero de pagina: %d\n", pagina);
+                printf("Desplazamiento: %d\n", desp);
+                printf("TLB: Hit\n\n");
+            }
         }
     }
     return 0;
